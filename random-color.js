7e2f1514d93b1a13a9d31771b4d3008f91b1421f
FarhanAfrad1 <farhan.tanmoy13@gmail.com>
2025-02-26 14:45:46 +0600

    also display the rgb code

--- random-color.js
+++ random-color.js
@@ -1,10 +1,15 @@
-
-function randomColor(){
-    return Math.floor(Math.random()*256)
+function randomColor() {
+  return Math.floor(Math.random() * 256);
 }
-const body = document.body
-const btn = document.getElementById('btn')
-btn.addEventListener('click',()=>{
-    body.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`
-    console.log(`rgb(${randomColor()},${randomColor()},${randomColor()})`)
-})
+const body = document.body;
+const btn = document.getElementById("btn");
+
+const p = document.createElement("p");
+p.style.fontSize = '20px'
+btn.addEventListener("click", (event) => {
+  console.log(event.target.parentNode);
+  body.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
+  p.innerText = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
+  event.target.parentNode.appendChild(p);
+  
+});
